34b11ff074ff23972de91eb81f0aa93862fcfa33
DonAbenz <xbossabenz@gmail.com>
2023-07-05 22:22:03 +0800

    disable register route

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,11 @@ Route::get('/', function () {
     return redirect('login');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => false, // Registration Routes...
+    'reset' => false, // Password Reset Routes...
+    'verify' => false, // Email Verification Routes...
+  ]);
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
