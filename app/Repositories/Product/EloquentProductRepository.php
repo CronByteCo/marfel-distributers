6dece738d7339b91c6bab8e20171cb66f151704e
DonAbenz <xbossabenz@gmail.com>
2023-07-06 22:14:14 +0800

    implement product repsitory

--- app/Repositories/Product/EloquentProductRepository.php
+++ app/Repositories/Product/EloquentProductRepository.php
@@ -16,4 +16,43 @@ class EloquentProductRepository extends AbstractEloquentRepository implements Pr
     {
         return Product::where('category_id', $categoryId)->paginate(10);
     }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function getProductByName($name)
+    {
+        return $this->model->where('name', $name)->first();
+    }
+
+    public function updateProductById($id, array $data)
+    {
+        return $this->model->where('id', $id)->update($data);
+    }
+
+    public function updateProductByName($name, array $data)
+    {
+        return $this->model->where('name', $name)->update($data);
+    }
+
+    public function delete($id)
+    {
+        $this->model->destroy($id);
+    }
+
+    public function getProductsByCategoryName($categoryName)
+    {
+        return $this->model->with('category')
+            ->whereHas('category', function ($builder) use ($categoryName) {
+                $builder->where('name', $categoryName);
+            })
+            ->get();
+    }
+
+    public function getAll()
+    {
+        return $this->model->with('category')->get();
+    }
 }
--- app/Repositories/Product/ProductRepositoryInterface.php
+++ app/Repositories/Product/ProductRepositoryInterface.php
@@ -6,6 +6,12 @@ use App\Models\Product;
 
 interface ProductRepositoryInterface
 {
-    public function all();
     public function allPaginatedByCategory($categoryId);
+    public function create(array $data);
+    public function getProductByName($name);
+    public function updateProductById($id, array $data);
+    public function updateProductByName($name, array $data);
+    public function delete($id);
+    public function getProductsByCategoryName($categoryName);
+    public function getAll();
 }
