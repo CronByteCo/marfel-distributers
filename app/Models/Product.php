f54f536f5404648f62b4e5b1e280a9366af49216
DonAbenz <xbossabenz@gmail.com>
2023-07-06 22:15:12 +0800

    implement product repo 2

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -23,4 +23,9 @@ class Product extends Model
     {
         return $this->belongsToMany(Order::class);
     }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
